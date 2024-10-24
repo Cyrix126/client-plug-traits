11713d9659765b69c0f7afbd63ed62c42543e24b
Louis-Marie Baer <lm@baermail.fr>
2024-10-24 18:17:21 +0200

    feat: feature seperation, borrow instead of owning, small method adjustment

--- src/lib.rs
+++ src/lib.rs
@@ -21,22 +21,43 @@ if #[cfg(feature="product_api_client")] {
 }
 cfg_if! {
 if #[cfg(feature="tasks_tracker_api_client")] {
-    pub trait NewTask {
+    // how to not duplicate the trait here but keep required trait on the where clause only for the feature bincode ?
+cfg_if! {
+
+if #[cfg(feature = "bincode")] {
+use bincode::{Decode, Encode};
+    pub trait NewTask where
+    Self: Encode + Decode
+    {
         fn duration(&self) -> u32;
-        fn scope(&self) -> String;
-        fn name(&self) -> String;
-        fn description(&self) -> String;
-        fn push_address(&self) -> Vec<Url>;
-        fn payload(&self) -> Vec<u8>;
-
+        fn scope(&self) -> &str;
+        fn name(&self) -> &str;
+        fn description(&self) -> &str;
+        fn push_address(&self) -> &[Url];
+        fn payload(&self) -> &[u8];
     }
+
+} else {
+    pub trait NewTask where
+    {
+        fn duration(&self) -> u32;
+        fn scope(&self) -> &str;
+        fn name(&self) -> &str;
+        fn description(&self) -> &str;
+        fn push_address(&self) -> &[Url];
+        fn payload(&self) -> &[u8];
+    }
+
+}
+}
     pub trait ResponseNewTask  {
-        fn location(&self) -> Url;
-        fn view_token(&self) -> String;
-        fn update_token(&self) -> String;
-        fn abort_token(&self) -> String;
+        fn location(&self) -> &Url;
+        fn view_token(&self) -> &str;
+        fn update_token(&self) -> &str;
+        fn abort_token(&self) -> &str;
     }
 
+    pub trait Task {}
     pub trait TaskTrackerClient
     where
         Self: Sized,
@@ -48,7 +69,7 @@ if #[cfg(feature="tasks_tracker_api_client")] {
             async fn update_task_progress(&self, task_location: &Url, new_progress: u8, token: Option<&str>) -> Result<(), impl std::error::Error>;
             async fn finish_task(&self, task_location: &Url, description_result: Option<&str>, payload_result: &[u8], token:Option<&str>) -> Result<(), impl std::error::Error>;
             async fn abort_task(&self, task_location: &Url, description_result: Option<&str>, payload_result: &[u8], token:Option<&str>) -> Result<(), impl std::error::Error>;
-            async fn get_task(&self, task_location: &Url, token: Option<&str>) -> Result<(), impl std::error::Error>;
+            async fn get_task(&self, task_location: &Url, token: Option<&str>) -> Result<impl Task, impl std::error::Error>;
         }
     }
 }
